61456471dc3dbb41e907e61dc14966758b5ba3dc
Afanasyev Pavel <bupyc9@gmail.com>
2017-04-03 23:40:53 +0300

    Onclick for "showAnswer" button

--- app/src/main/java/ru/bupyc9/geoquiz/CheatActivity.java
+++ app/src/main/java/ru/bupyc9/geoquiz/CheatActivity.java
@@ -4,10 +4,15 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class CheatActivity extends AppCompatActivity {
     public static final String EXTRA_ANSWER_IS_TRUE = "ru.bupyc9.geoquiz.answer_is_true";
     private boolean mAnswerIsTrue;
+    private TextView mAnswerTextView;
+    private Button mShowAnswer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +20,19 @@ public class CheatActivity extends AppCompatActivity {
         setContentView(R.layout.activity_cheat);
 
         mAnswerIsTrue = getIntent().getBooleanExtra(EXTRA_ANSWER_IS_TRUE, false);
+        mAnswerTextView = (TextView) findViewById(R.id.answerTextView);
+        mShowAnswer = (Button) findViewById(R.id.showAnsweButton);
+
+        mShowAnswer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mAnswerIsTrue) {
+                    mAnswerTextView.setText(R.string.true_button);
+                } else {
+                    mAnswerTextView.setText(R.string.false_button);
+                }
+            }
+        });
     }
 
     public static Intent newIntent(Context packageContext, boolean answerIsTrue) {
