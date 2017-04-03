b3d6f84f93f9629dbbb9b20e66b797952f9a7ea2
Afanasyev Pavel <bupyc9@gmail.com>
2017-04-03 23:27:23 +0300

    Add class "cheat" activity

---
+++ app/src/main/java/ru/bupyc9/geoquiz/CheatActivity.java
@@ -0,0 +1,12 @@
+package ru.bupyc9.geoquiz;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class CheatActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cheat);
+    }
+}
